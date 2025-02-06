68a0a709fed50bc922f6397ebe136ec187d8dd5b
sat0sh1c <114339498+sat0sh1c@users.noreply.github.com>
2025-02-07 03:15:26 +0500

    addded lspconfig.lua

---
+++ lua/configs/lspconfig.lua
@@ -0,0 +1,27 @@
+-- load defaults i.e lua_lsp
+require("nvchad.configs.lspconfig").defaults()
+
+local lspconfig = require "lspconfig"
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+require'lspconfig'.neocmake.setup {
+  capabilities = capabilities,
+}
+
+require'lspconfig'.neocmake.setup{}
+require 'lspconfig'.clangd.setup{}
+
+local servers = { "html", "cssls", "clangd", "neocmake"}
+local nvlsp = require "nvchad.configs.lspconfig"
+
+-- lsps with default config
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = nvlsp.on_attach,
+    on_init = nvlsp.on_init,
+    capabilities = nvlsp.capabilities,
+  }
+end
+
