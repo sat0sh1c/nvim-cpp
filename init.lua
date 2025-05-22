a54075b0fff6475db0199e3f12ce72c9edd80038
sat0sh1c <114339498+sat0sh1c@users.noreply.github.com>
2025-05-23 00:35:59 +1000

    replaced question with path to debug with envinroment variable and built script, moved debug logic to plugins dir

--- init.lua
+++ init.lua
@@ -5,8 +5,8 @@ vim.g.mapleader = " "
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
 if not vim.uv.fs_stat(lazypath) then
-  local repo = "https://github.com/folke/lazy.nvim.git"
-  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
+   local repo = "https://github.com/folke/lazy.nvim.git"
+   vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
 end
 
 vim.opt.rtp:prepend(lazypath)
@@ -15,14 +15,14 @@ local lazy_config = require "configs.lazy"
 
 -- load plugins
 require("lazy").setup({
-  {
-    "NvChad/NvChad",
-    lazy = false,
-    branch = "v2.5",
-    import = "nvchad.plugins",
-  },
+   {
+      "NvChad/NvChad",
+      lazy = false,
+      branch = "v2.5",
+      import = "nvchad.plugins",
+   },
 
-  { import = "plugins" },
+   { import = "plugins" },
 }, lazy_config)
 -- load theme
 dofile(vim.g.base46_cache .. "defaults")
@@ -32,34 +32,6 @@ require "options"
 require "nvchad.autocmds"
 require "plugins.debugger"
 
-local dap = require "dap"
-local dapui = require "dapui"
-dapui.setup()
-
-dap.adapters.codelldb = {
-   type = "server",
-   port = "${port}",
-   executable = {
-      command = "codelldb",
-      args = { "--port", "${port}" },
-   },
-}
-
-dap.configurations.cpp = {
-   {
-      name = "Launch file",
-      type = "codelldb",
-      request = "launch",
-      program = function()
-         return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-      end,
-      cwd = "${workspaceFolder}",
-      args = {},
-      stopOnEntry = false,
-   },
-}
-
-
 vim.schedule(function()
-  require "mappings"
+   require "mappings"
 end)
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,34 +1,82 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
-    opts = require "configs.conform",
-  },
+   {
+      "stevearc/conform.nvim",
+      event = "BufWritePre", -- uncomment for format on save
+      opts = require "configs.conform",
+   },
 
-  -- These are some examples, uncomment them if you want to see them work!
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "configs.lspconfig"
-    end,
-    ensure_installed =
-    {
-      "Decodetalkers/neocmakelsp",
-      "clangd/clangd",
-      "hrsh7th/nvim-cmp",
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-    }
-  },
+   -- These are some examples, uncomment them if you want to see them work!
+   {
+      "neovim/nvim-lspconfig",
+      config = function()
+         require "configs.lspconfig"
+         local dap = require "dap"
+         local dapui = require "dapui"
+         dapui.setup()
 
-  {
-  	"nvim-treesitter/nvim-treesitter",
-  	opts = {
-  		ensure_installed = {
-  			"vim", "lua", "vimdoc",
-       "html", "css", "cmake", "cpp"
-  		},
-  	},
-  },
+         dap.adapters.codelldb = {
+            type = "server",
+            port = "${port}",
+            executable = {
+               command = "codelldb",
+               args = { "--port", "${port}" },
+            },
+         }
+
+         function Is_process_running(name)
+            local handle = io.popen("pgrep -f " .. name)
+            local pid = tonumber(handle:read "*a")
+            if pid == nil then
+               handle:close()
+               return false
+            end
+            handle:close()
+            return true
+         end
+         dap.configurations.cpp = {
+            {
+               name = "Launch file",
+               type = "codelldb",
+               request = "launch",
+               program = function()
+                  os.execute "kitty --detach ~/./build.sh"
+                  while true do
+                     local is_running = Is_process_running "build.sh"
+                     if is_running == false then
+                        break
+                     end
+                     os.execute "sleep 1s"
+                  end
+                  return vim.fn.getcwd() .. "/build/" .. os.getenv "CURRENT_PROJECT"
+               end,
+               cwd = "${workspaceFolder}",
+               args = {},
+               stopOnEntry = false,
+            },
+         }
+      end,
+      ensure_installed = {
+         "Decodetalkers/neocmakelsp",
+         "clangd/clangd",
+         "hrsh7th/nvim-cmp",
+         "hrsh7th/cmp-nvim-lsp",
+         "hrsh7th/cmp-buffer",
+         "hrsh7th/cmp-path",
+      },
+   },
+
+   {
+      "nvim-treesitter/nvim-treesitter",
+      opts = {
+         ensure_installed = {
+            "vim",
+            "lua",
+            "vimdoc",
+            "html",
+            "css",
+            "cmake",
+            "cpp",
+         },
+      },
+   },
 }
